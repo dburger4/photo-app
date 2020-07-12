e5c59bb07745b88fc4c78b09aeb0b414da30fdc4
Dan Burger <burger5@purdue.edu>
2020-07-11 23:13:55 -0400

    Payment Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_032710) do
+ActiveRecord::Schema.define(version: 2020_07_12_030416) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
